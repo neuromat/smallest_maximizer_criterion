376153c11720806501a4ba9b38d3515656154c66
Arthur Tofani <gramofone@gmail.com>
2021-02-26 17:35:00 -0300

    fixed run sh

--- examples/run_ml_fast.sh
+++ examples/run_ml_fast.sh
@@ -2,10 +2,10 @@ conda activate py37 && python simulation_study.py model1_5000_ml \
 	--model model1 \
 	--df csizar_and_talata \
 	--resamples 200 \
-	--sample_size 10000 \
+	--sample_size 5000 \
 	--num_cores 20 \
 	--penalty_interval 0 500 \
-	--scan_offset 0 \
+	--scan_offset 0
 
 conda activate py37 && python simulation_study.py model1_10000_ml \
 	--model model1 \
@@ -14,7 +14,7 @@ conda activate py37 && python simulation_study.py model1_10000_ml \
 	--sample_size 10000 \
 	--num_cores 20 \
 	--penalty_interval 0 500 \
-	--scan_offset 0 \
+	--scan_offset 0
 
 conda activate py37 && python simulation_study.py model1_20000_ml \
 	--model model1 \
@@ -23,16 +23,16 @@ conda activate py37 && python simulation_study.py model1_20000_ml \
 	--sample_size 20000 \
 	--num_cores 20 \
 	--penalty_interval 0 500 \
-	--scan_offset 0 \
+	--scan_offset 0
 
 conda activate py37 && python simulation_study.py model2_5000_ml \
 	--model model2 \
 	--df csizar_and_talata \
 	--resamples 200 \
-	--sample_size 10000 \
+	--sample_size 5000 \
 	--num_cores 20 \
 	--penalty_interval 0 500 \
-	--scan_offset 0 \
+	--scan_offset 0
 
 conda activate py37 && python simulation_study.py model2_10000_ml \
 	--model model2 \
@@ -41,7 +41,7 @@ conda activate py37 && python simulation_study.py model2_10000_ml \
 	--sample_size 10000 \
 	--num_cores 20 \
 	--penalty_interval 0 500 \
-	--scan_offset 0 \
+	--scan_offset 0
 
 conda activate py37 && python simulation_study.py model2_20000_ml \
 	--model model2 \
@@ -50,4 +50,4 @@ conda activate py37 && python simulation_study.py model2_20000_ml \
 	--sample_size 20000 \
 	--num_cores 20 \
 	--penalty_interval 0 500 \
-	--scan_offset 0 \
+	--scan_offset 0
